3c5ca07bfb86a7ebbf42a3f344889dbaed81845d
luxlinna <luxlinna@gmail.com>
2022-11-15 22:08:53 +0100

    Initial Commit

---
+++ FBook/FBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBookApp.swift
+//  FBook
+//
+//  Created by Linna Lux on 2022-11-15.
+//
+
+import SwiftUI
+
+@main
+struct FBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
